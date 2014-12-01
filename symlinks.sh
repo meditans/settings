627f01a9cfaa9296f0f7e663acd6da4b55cf563b
Carlo Nucera <meditans@gmail.com>
2014-12-01 22:37:56 +0100

    Added xmonad configuration file and symlinking

--- symlinks.sh
+++ symlinks.sh
@@ -8,3 +8,12 @@ scriptDir=$( cd "$( dirname "$0" )" && pwd )
 # Symlink per i file di configurazione globali
 rm -r /etc/nixos
 ln -s "$scriptDir/nixos" /etc/nixos
+
+# Symlink per i file di configurazione di xmonad
+if [ -d "$HOME/.xmonad" ]
+then
+   rm -r "$HOME/.xmonad"
+fi
+
+mkdir "$HOME/.xmonad"
+ln -s "$scriptDir/xmonad/xmonad.hs" "$HOME/.xmonad/xmonad.hs"
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,13 @@
+import XMonad
+import XMonad.Util.EZConfig
+ 
+main = xmonad $ defaultConfig
+  { modMask = mod4Mask   -- super instead of alt
+  , terminal = "terminator"
+  , borderWidth = 0
+  }
+  `additionalKeysP`
+  [ ("M-e", spawn "emacs")
+  , ("M-f", spawn "firefox")
+  , ("M-t", spawn "texmacs")
+  ]
