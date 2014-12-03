9f93de69e8d3e1fb42c260cb53c646f65a7a2d33
Carlo Nucera <meditans@gmail.com>
2014-12-03 11:02:23 +0100

    Added comment on adblockplus installation

--- conkerorSymLink.sh
+++ conkerorSymLink.sh
@@ -2,7 +2,9 @@
 
 # Script che serve per configurare le mie impostazioni del browser
 # conkeror. Il bello e' che posso symlinkare direttamente dentro la
-# mia cartella il file .conkerorrc.
+# mia cartella il file .conkerorrc. Ricordiamo che poi posso
+# installare adblockplus dal sito dell'estensione, anche se sembra non
+# funzionare, e aggiungere i filtri da M-x extensions.
 
 scriptDir=$( cd "$( dirname "$0" )" && pwd )
 
