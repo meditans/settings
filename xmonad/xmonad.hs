1573a8604ac6aebbabd556d6e24361273f62f1bb
Carlo Nucera <meditans@gmail.com>
2014-12-05 16:03:40 +0100

    Added xmonad keybinding for emacsclient

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,13 +1,13 @@
 import XMonad
 import XMonad.Util.EZConfig
- 
+
 main = xmonad $ defaultConfig
   { modMask = mod4Mask   -- super instead of alt
   , terminal = "terminator"
   , borderWidth = 0
   }
   `additionalKeysP`
-  [ ("M-e", spawn "emacs")
+  [ ("M-e", spawn "emacsclient -c")
   , ("M-f", spawn "firefox")
   , ("M-c", spawn "conkeror")
   ]
