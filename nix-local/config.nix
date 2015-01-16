d64a87c6b7f3dc673e99a50a643f254bff2c392d
Carlo Nucera <meditans@gmail.com>
2015-01-16 22:39:49 +0000

    Updated modern version in repo

--- nix-local/config.nix
+++ nix-local/config.nix
@@ -1,33 +1,76 @@
-{ packageOverrides = pkgs: rec {
-    haskellPackages =
-      pkgs.recurseIntoAttrs (pkgs.haskellPackages.override {
-        extension = self : super :
-        let callPackage = self.callPackage;
-        in {
-          musicDynamicsLiteral = callPackage /home/carlo/code/nix-packaging/music-dynamics-literal/default.nix {};
-          musicPitchLiteral = callPackage /home/carlo/code/nix-packaging/music-pitch-literal/default.nix {};
-          prettify = callPackage /home/carlo/code/nix-packaging/prettify/default.nix {};
-          lilypond = callPackage /home/carlo/code/nix-packaging/lilypond/default.nix {};
-          reverseApply = callPackage /home/carlo/code/nix-packaging/reverse-apply/default.nix {};
-          musicxml2 = callPackage /home/carlo/code/nix-packaging/musicxml2/default.nix {};
-          typeUnary = callPackage /home/carlo/code/nix-packaging/type-unary/default.nix {};
-          applicativeNumbers = callPackage /home/carlo/code/nix-packaging/applicative-numbers/default.nix {};
-          ty = callPackage /home/carlo/code/nix-packaging/ty/default.nix {};
-          abcnotation = callPackage /home/carlo/code/nix-packaging/abcnotation/default.nix {};
-          musicPitch = callPackage /home/carlo/code/nix-packaging/music-pitch/default.nix {};
-          dataInterval = callPackage /home/carlo/code/nix-packaging/data-interval/default.nix {};
-          extendedReals = callPackage /home/carlo/code/nix-packaging/extended-reals/default.nix {};
-          lattices = callPackage /home/carlo/code/nix-packaging/lattices/default.nix {};
-          positive = callPackage /home/carlo/code/nix-packaging/positive/default.nix {};
-          musicParts = callPackage /home/carlo/code/nix-packaging/music-parts/default.nix {};
-          musicDynamics = callPackage /home/carlo/code/nix-packaging/music-dynamics/default.nix {};
-          average = callPackage /home/carlo/code/nix-packaging/average/default.nix {};
-          romanNumerals = callPackage /home/carlo/code/nix-packaging/roman-numerals/default.nix {};
-          musicScore = callPackage /home/carlo/code/nix-packaging/music-score/default.nix {};
-          HCodecs = callPackage /home/carlo/code/nix-packaging/HCodecs/default.nix {};
-          monadplus = callPackage /home/carlo/code/nix-packaging/monadplus/default.nix {};
-          musicPreludes = callPackage /home/carlo/code/nix-packaging/music-preludes/default.nix {};
-          musicArticulation = callPackage /home/carlo/code/nix-packaging/music-articulation/default.nix {};
-        };
-      });
-};}
+# ~/.nixpkgs/config.nix
+
+{
+  packageOverrides = super:
+  let
+    self = super.pkgs;
+    haskellPackagingDir = "/home/carlo/code/nix-packaging/";
+    locate = pkg: haskellPackagingDir + pkg + "/default.nix";
+  in
+  {
+    haskellDevEnv = self.haskellPackages.ghcWithPackages (p: with p; [
+      # Libraries 
+      QuickCheck
+      basicPrelude
+      cheapskate
+      classyPrelude
+      clay
+      diagrams
+      fgl
+      fglVisualize
+      graphviz
+      hakyll
+      helios
+      hi
+      hspec
+      listExtras
+      markdown
+      mtl
+      musicSuite
+      positive
+      prettify
+      random
+      split
+      text
+      transformers
+      unificationFd
+      unorderedContainers
+      vector
+
+      # tools
+      cabalInstall
+      ghcMod
+    ]);
+
+    haskellPackages = super.haskellPackages.override {
+      extension = self: super: {
+        abcnotation = self.callPackage (locate "abcnotation") {};
+        applicativeNumbers = self.callPackage (locate "applicative-numbers") {};
+        average = self.callPackage (locate "average") {};
+        dataInterval = self.callPackage (locate "data-interval") {};
+        extendedReals = self.callPackage (locate "extended-reals") {};
+        HCodecs = self.callPackage (locate "HCodecs") {};
+        helios = self.callPackage "/home/carlo/code/haskell/helios/default.nix" {}; 
+        lattices = self.callPackage (locate "lattices") {};
+        lilypond = self.callPackage (locate "lilypond") {};
+        monadplus = self.callPackage (locate "monadplus") {};
+        musicArticulation = self.callPackage (locate "music-articulation") {};
+        musicDynamics = self.callPackage (locate "music-dynamics") {};
+        musicDynamicsLiteral = self.callPackage (locate "music-dynamics-literal") {};
+        musicParts = self.callPackage (locate "music-parts") {};
+        musicPitch = self.callPackage (locate "music-pitch") {};
+        musicPitchLiteral = self.callPackage (locate "music-pitch-literal") {};
+        musicPreludes = self.callPackage (locate "music-preludes") {};
+        musicScore = self.callPackage (locate "music-score") {};
+        musicSuite = self.callPackage (locate "music-suite") {};
+        musicxml2 = self.callPackage (locate "musicxml2") {};
+        positive = self.callPackage "/home/carlo/code/haskell/positive/default.nix" {};
+        prettify = self.callPackage (locate "prettify") {};
+        reverseApply = self.callPackage (locate "reverse-apply") {};
+        romanNumerals = self.callPackage (locate "roman-numerals") {};
+        ty = self.callPackage (locate "ty") {};
+        typeUnary = self.callPackage (locate "type-unary") {};
+      };
+    };
+  };
+}
