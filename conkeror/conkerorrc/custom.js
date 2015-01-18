eeb9920c5003297ed1f71aa8aa7bfa6bb9b79c2f
Carlo Nucera <meditans@gmail.com>
2015-01-18 16:10:22 +0000

    Added viewmarks support

--- conkeror/conkerorrc/custom.js
+++ conkeror/conkerorrc/custom.js
@@ -1,1 +1,7 @@
 // add your custom additions here
+// Questa parte serve effettivamente per installare viewmarks
+interactive("viewmarks",
+    "Open ViewMarks window.",
+    function (I) {
+        make_chrome_window('chrome://viewmarks/content/viewmark.xul');
+    });
